52a883f61e3d051d3e8d3b8fe887ba28034c7fac
Soar <yhx5768@hotmail.com>
2017-07-09 19:47:04 +0800

    changed proxy software and settings.

--- setup_proxy.sh
+++ setup_proxy.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+source ./apt_getw.sh
+
+pip install shadowsocks
 
 cp -r ./base/shadowsocks ~/Tools
 
-chmod 754 ~/Tools/shadowsocks/shadowsocks-local-linux64-1.1.4
-
-mkdir -p ~/.config/autostart/
-cp ./base/shadowsocks/shadowsocks-local-linux64-1.1.4.desktop ~/.config/autostart/
-
-nohup ~/Tools/shadowsocks/shadowsocks-local-linux64-1.1.4 > /dev/null & 
+sslocal -c ~/program/shadowsocks/config.json start &
 
 cp -r ./base/proxy/.proxychains ~/
 
@@ -17,6 +15,6 @@ sudo sed -i "s/LD_PRELOAD=libproxychains.so.3/LD_PRELOAD=\/usr\/lib\/x86_64-linu
 
 sudo apt-get -y install polipo
 
-sudo cp base/polipo/config /etc/polipo 
+sudo cp base/polipo/config /etc/polipo
 
 sudo service polipo restart
