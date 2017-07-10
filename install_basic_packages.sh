9953f92c1bb42532121d35a76002d125aef947d1
Soar <yhx5768@hotmail.com>
2017-07-10 20:45:12 +0800

    add vim

--- install_basic_packages.sh
+++ install_basic_packages.sh
@@ -3,4 +3,4 @@ source ./apt_getw.sh
 
 sudo dpkg --add-architecture i386
 apt_getw update
-apt_getw -y install curl aria2 python-pip exfat-fuse exfat-utils
+apt_getw -y install curl aria2 python-pip vim exfat-fuse exfat-utils
